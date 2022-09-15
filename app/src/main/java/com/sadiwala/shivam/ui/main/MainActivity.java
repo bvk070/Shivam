bbee0b5db7b4642a49e4686c03128a43ed62e7ba
Bhavik Sadiwala <bhavik@getvymo..com>
2022-09-15 14:43:52 +0530

    - bottom nav setup is done

--- app/src/main/java/com/sadiwala/shivam/ui/main/MainActivity.java
+++ app/src/main/java/com/sadiwala/shivam/ui/main/MainActivity.java
@@ -2,6 +2,11 @@ package com.sadiwala.shivam.ui.main;
 
 import android.os.Bundle;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sadiwala.shivam.R;
 import com.sadiwala.shivam.ui.BaseActivity;
 
@@ -12,9 +17,9 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
+        NavController navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
     }
 
